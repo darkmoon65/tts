adab35550b22b493928bbc6d74dfea61c6e4d04b
cesar alonso <icetom6@hotmail.com>
2021-03-20 23:33:39 -0500

    adding dates

--- ttsCode.php
+++ ttsCode.php
@@ -7,7 +7,7 @@ $agi = new AGI();
 
 function consultaIbm($qr){
         $apiKey = 'R_3F1fdc_XkJ9WX3VZ6S8y-L_q_wN_QOz1BGmGTJdVaH';
-        $apiUrl = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/7574d201-24f6-40ce-89c3-f4c816ba7edd/v1/synthesize?accept=audio%2Fwav&text='.$qr.'&voice=es-ES_EnriqueV3Voice';
+        $apiUrl = 'https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/7574d201-24f6-40ce-89c3-f4c816ba7edd/v1/synthesize?accept=audio%2Fmulaw%3Brate%3D8000&text='.$qr.'&voice=es-ES_EnriqueV3Voice';
     
         $curl = curl_init();
         curl_setopt ( $curl, CURLOPT_URL, $apiUrl);
@@ -20,6 +20,7 @@ function consultaIbm($qr){
 }
 
 // LLamada a la api de consultas
+$wDir="/gravaciones/citas/tts/";
 if (isset($argv[1])){
     $head = array();
     $head[] = 'Content-type: application/json';
@@ -51,27 +52,71 @@ if (isset($argv[1])){
         
         $result = consultaIbm($encodeMesage);
         echo("listo");
-        $wr = fopen('audio2.wav',"w");
+        $wr = fopen($wDir.'audio.ulaw',"w");
         fwrite($wr, $result);
+        play_date();
+        $agi->answer();
+        $agi->stream_file($wDir."audio");
     }else{
         echo $newData['mensaje'];
         $message = $newData['mensaje'];
         $encodeMesage = urlencode($message);
         $result = consultaIbm($encodeMesage);
-        echo("listo 2");
-        $wr = fopen('audio3.wav',"w");
+        $wr = fopen($wDir.'audio.ulaw',"w");
         fwrite($wr, $result);
+        $agi->answer();
+        $agi->stream_file($wDir."audio");
+
     }
 }else{
     echo "Ingrese rut";
     $message = "Ingrese rut";
     $encodeMesage = urlencode($message);
     $result = consultaIbm($encodeMesage);
-    echo("listo 2");
-    $wr = fopen('audio3.wav',"w");
+    $wr = fopen($wDir.'audio.ulaw',"w");
     fwrite($wr, $result);
     $agi->answer();
-    $agi->stream_file($work_dir."/audio3");
+    $agi->stream_file($wDir."audio");
+}
+
+
+function play_date($fechaP,$horaP){
+    $tFecha = explode(" ", $fechaP);
+    $tHora = explode(" ", $horaP);
+
+    $td = explode("/", $tFecha[0]);
+    $dia = intval("${td[0]}");
+    $mes = intval("${td[1]}") - 1;
+
+    $th = explode(":", $tHora[0);
+    $hora = $th[0] ? intval("${th[0]}") : 0;
+    $minuto = $th[1] ? intval("${th[1]}") : 0;
+
+    $agi->answer();
+    PlayTimePart($agi,$dia);
+    $agi->stream_file("digits/pt-de");
+    $agi->stream_file("digits/mon-".$mes);
+    $agi->stream_file("digits/pt-as");
+    PlayTimePart($agi,$hora);
+    $agi->stream_file("letters/e");
+    PlayTimePart($agi,$minuto);
+
+    function PlayTimePart($agi,$t1){
+        if($t1 > 20){
+            if(substr($t1, -1) == 0){
+                $agi->stream_file("digits/".$t1);
+            }
+            else {
+                $agi->stream_file("digits/".substr($t1,0,1)."0");
+                $agi->stream_file("letters/e");
+                $agi->stream_file("digits/".substr($t1, -1));
+            }
+        }
+        else {
+            $agi->stream_file("digits/".$t1);
+        }
+    }
+
 }
 
 ?>
